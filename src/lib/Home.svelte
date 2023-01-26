d35d639cf1f55d14888a52636a90b21c5b3dfdb9
Layona-x <109739779+Layona-x@users.noreply.github.com>
2023-01-26 08:40:43 +0300

    Add files via upload

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,1 +1,9 @@
+<script>
+ import Navbar from "../components/Header.svelte"
+ import Statuses from "../components/Statuses.svelte"
+ import Posts from "../components/Posts.svelte"
+</script>
 
+<Navbar />
+<Statuses />
+<Posts />
